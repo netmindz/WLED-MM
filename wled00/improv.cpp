30b05fd9f5bea128e14e272bcfd58ca34bdd56de
Will Tatam <will@netmindz.net>
2025-11-08 09:16:13 +0000

    rename to WLEDNetwork

--- wled00/improv.cpp
+++ wled00/improv.cpp
@@ -95,7 +95,7 @@ void handleImprovPacket() {
             case ImprovRPCType::Request_State: {
               uint8_t improvState = 0x02; //authorized
               if (WLED_WIFI_CONFIGURED) improvState = 0x03; //provisioning
-              if (Network.isConnected()) improvState = 0x04; //provisioned
+              if (WLEDNetwork.isConnected()) improvState = 0x04; //provisioned
               sendImprovStateResponse(improvState, false);
               if (improvState == 0x04) sendImprovIPRPCResult(ImprovRPCType::Request_State);
               break;
