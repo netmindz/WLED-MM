5664a0a973277df9017d9dc5521d90409653749d
Frank <91616163+softhack007@users.noreply.github.com>
2025-11-24 00:38:52 +0100

    Comment out WLED upstream URL replacement
    Commented out the line replacing the WLED upstream URL to prevent breaking credits.

--- tools/cdata.js
+++ tools/cdata.js
@@ -63,7 +63,7 @@ function adoptVersionAndRepo(html) {
     // Replace we
     html = strReplace(html, "https://github.com/atuline/WLED", repoUrl);
     html = strReplace(html, "https://github.com/Aircoookie/WLED", repoUrl);
-    html = strReplace(html, "https://github.com/wled-dev/WLED", repoUrl);
+    // html = strReplace(html, "https://github.com/wled-dev/WLED", repoUrl); // replacing upstream break "credits"
     // html = strReplace(html, "https://github.com/wled/WLED", repoUrl);
     // html = strReplace(html, "https://github.com/MoonModules/WLED", repoUrl); //WLEDMM
     // html = strReplace(html, "https://github.com/MoonModules/WLED-MM", repoUrl); //WLEDMM - not necessary to replace ourselves ;-)
